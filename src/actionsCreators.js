878ac9c3348d4d9d8a57eb3695107227565ecfbd
Winston Flores <winstonflores30@hotmail.com>
2018-08-04 00:19:13 -0500

    Llamamas asíncronas con json-server, react-redux-thunk y axios

--- src/actionsCreators.js
+++ src/actionsCreators.js
@@ -1,3 +1,18 @@
+import axios from 'axios';
+
+const loadProducts = () => {
+    return dispatch => {
+        return axios.get("http://localhost:3001/Products")
+        .then(response => {
+            dispatch({
+                type: "REPLACE_PRODUCTS",
+                products: response.data
+            });
+        });
+    };
+};
+
+
 const addToCart = product => {
     return {
         type: "ADD_TO_CART",
@@ -12,4 +27,4 @@ const removeFromCart = product => {
     };
 };
 
-export { addToCart, removeFromCart };
+export { loadProducts, addToCart, removeFromCart };
