d131de83a50ab6935b41f4c4ded51cfa78d8bdd4
Winston Flores <winstonflores30@hotmail.com>
2019-01-25 15:28:11 -0500

    Version del Repo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <Navbar inverse staticTop>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">Ecommerce</a>
+              <a href="#">Ecommerce DeployGitHub</a>
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
