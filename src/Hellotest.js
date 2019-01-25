74803dd078013123d7e5c2cdd98f3aed41f1663e
Winston Flores <winstonflores30@hotmail.com>
2019-01-25 15:24:02 -0500

    subiendo Cambios

---
+++ src/Hellotest.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import products from './components/ProductList';
+import { shallow } from 'enzyme';
+
+/*
+it('renders', () => {
+  const wrapper = shallow(<Hello name="Jack" />);
+  expect(wrapper.find('p').text()).toEqual('Hello, Jack!');
+});
+
+it('renders an `.icon-star`', () => {
+  const wrapper = shallow(<products />);
+  expect(wrapper.find('.icon-star')).to.have.length(1);
+});
+*/
